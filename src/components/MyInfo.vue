1dd072e3a7661a72f9035ffc54add0bcadffb943
xuedagong <xuedagong@znlh.com>
2021-09-10 13:55:43 +0800

    初始化项目

---
+++ src/components/MyInfo.vue
@@ -0,0 +1,14 @@
+<script setup  lang="ts">
+ const msg="nihao"
+ import { defineProps, reactive } from "vue";
+    const props = defineProps({
+    email: String
+    });
+</script>
+
+<template>
+    <div>
+        <p> 我的名称：{{msg}}</p>
+        <p> 邮箱：{{email}}</p>
+    </div>
+</template>
